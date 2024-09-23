63bc6cdab7e58108ffb44c68a5467e22ed14a8e0
Andrii Skega <Game.a.s.production@gmail.com>
2024-09-23 12:50:45 +0200

    fix: add cors settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['utfs.io'],
-  },
+	images: {
+		domains: ['utfs.io'],
+	},
+	async headers() {
+		return [
+			{
+				// Применяем заголовки ко всем маршрутам
+				source: '/(.*)',
+				headers: [
+					{
+						key: 'Access-Control-Allow-Origin',
+						value: 'https://fashioncase.vercel.app', // Разрешаем запросы с этого домена
+					},
+					{
+						key: 'Access-Control-Allow-Methods',
+						value: 'GET, POST, OPTIONS', // Методы, которые разрешены
+					},
+					{
+						key: 'Access-Control-Allow-Headers',
+						value: 'X-Requested-With, Content-Type, Authorization', // Разрешённые заголовки
+					},
+				],
+			},
+		]
+	},
 }
 
 export default nextConfig
